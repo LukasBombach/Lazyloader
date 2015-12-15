418ac2d147f4afbb61bd93b51192f446a2f73ff5
Lukas Bombach <lukas.bombach@googlemail.com>
2015-12-15 15:00:26 +0100

    Class implementation

--- lazyloader-class.js
+++ lazyloader-class.js
@@ -3,18 +3,23 @@
 /**
  *
  * @param {...string} files Any number of URLs to load
- * @param callback The callback the will be called when
+ * @param {Function} callback The callback the will be called when
  *     all files have been loaded.
  * @constructor
  */
 function LazyLoader(files, callback) {
-  this._files = Array.prototype.slice.call(arguments, 0, -1);
-  this._callback = Array.prototype.slice.call(arguments, -1);
-  this._numFilesLoading = 0;
-  this._load();
+
+  var args = Array.prototype.slice.call(arguments);
+  callback = this._isFunction(args[args.length - 1]) ? args.pop() : this._noop;
+
+  this.setCallback(callback);
+  this.setFiles(args);
+
+  this.load();
+
 }
 
-(function() {
+(function () {
 
   /**
    *
@@ -46,13 +51,32 @@ function LazyLoader(files, callback) {
 
   /**
    *
+   * @param {Function} callback The callback the will be called when
+   *     all files have been loaded.
+   * @returns {LazyLoader} This instance
+   */
+  this.setCallback = function (callback) {
+    return this;
+  };
+
+  /**
+   *
+   * @param {string[]|...string} files Any number of URLs to load
+   * @returns {LazyLoader} This instance
+   */
+  this.setFiles = function (files) {
+    return this;
+  };
+
+  /**
+   *
    * @returns {LazyLoader} This instance
    * @private
    */
-  this._load = function () {
+  this.load = function () {
     var len = this._files.length;
-    for (var i = 0; i < len; i++)
-      this._loadFile(this._files[i])
+    this._numFilesLoading = 0;
+    for (var i = 0; i < len; i++) this._loadFile(this._files[i])
     return this;
   };
 
@@ -63,24 +87,38 @@ function LazyLoader(files, callback) {
    * @private
    */
   this._loadFile = function (url) {
-
-    if (this._getFileType(url) === this._TYPE_JS) {
-      this._numFilesLoading++;
-      this._appendScript(url)
-    }
-
-    if (this._getFileType(url) === this._TYPE_CSS) {
-      this._numFilesLoading++;
-      this._appendStylesheet(url)
-    }
-
+    if (this._getFileType(url) === this._TYPE_JS) this._loadScript(url);
+    if (this._getFileType(url) === this._TYPE_CSS) this._loadStylesheet(url);
     return this;
-
   };
 
   /**
    *
-   * @param url
+   * @param {string} url
+   * @returns {LazyLoader} This instance
+   * @private
+   */
+  this._loadScript = function (url) {
+    this._numFilesLoading++;
+    this._appendScript(url);
+    return this;
+  };
+
+  /**
+   *
+   * @param {string} url
+   * @returns {LazyLoader} This instance
+   * @private
+   */
+  this._loadStylesheet = function (url) {
+    this._numFilesLoading++;
+    this._appendStylesheet(url);
+    return this;
+  };
+
+  /**
+   *
+   * @param {string} url
    * @returns {LazyLoader} This instance
    * @private
    */
@@ -90,6 +128,23 @@ function LazyLoader(files, callback) {
     script.src = url;
     script.onload = this._decrementAndCallGlobalCallback.bind(this);
     this._appendToHead(script);
+    return this;
+  };
+
+  /**
+   *
+   * @param {string} url
+   * @returns {LazyLoader} This instance
+   * @private
+   */
+  this._appendStylesheet = function (url) {
+    var link = document.createElement("link");
+    link.href = url;
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.onload = this._decrementAndCallGlobalCallback.bind(this);
+    link.onreadystatechange = this._getOnReadyStateChangeCallback();
+    this._appendToHead(link);
     return this;
   };
 
@@ -110,7 +165,7 @@ function LazyLoader(files, callback) {
    * @returns {string|null}
    * @private
    */
-  this._getFileType = function(url) {
+  this._getFileType = function (url) {
     if (this._JS_REGEX.test(url)) return this._TYPE_JS;
     if (this._CSS_REGEX.test(url)) return this._TYPE_CSS;
     return null;
@@ -118,13 +173,44 @@ function LazyLoader(files, callback) {
 
   /**
    *
+   * @returns {Function}
+   * @private
+   */
+  this._getOnReadyStateChangeCallback = function () {
+    var self = this;
+    return function () {
+      if (['loaded', 'complete'].indexOf(this.readyState) > -1)
+        self._decrementAndCallGlobalCallback();
+    };
+  };
+
+  /**
+   *
    * @returns {LazyLoader} This instance
    * @private
    */
-  this._decrementAndCallGlobalCallback = function() {
-    if(--this._numFilesLoading === 0) this._callback();
+  this._decrementAndCallGlobalCallback = function () {
+    if (--this._numFilesLoading === 0) this._callback();
     return this;
-  }
+  };
+
+  /**
+   *
+   * @param functionToCheck
+   * @returns {*|boolean}
+   * @private
+   */
+  this._isFunction = function(functionToCheck) {
+    var getType = {};
+    return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
+  };
+
+  /**
+   *
+   * @private
+   */
+  this._noop = function () {
+  };
 
 }).call(LazyLoader.prototype);
 
